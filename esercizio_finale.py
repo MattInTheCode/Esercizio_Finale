ae3f2b9decf0abf26b3fdceb50a253b81e1e46b9
MattInTheCode <esposito0mattia@gmail.com>
2025-02-24 12:01:09 +0100

    2° branch con generazione DF
    Generazione del data frame

--- esercizio_finale.py
+++ esercizio_finale.py
@@ -15,8 +15,23 @@ visitatori = np.random.normal(media, std, numero_giorni)
 
 # trend crescente nel tempo
 trend = np.linspace(0, 300, numero_giorni)
-visitatori = visitatori + trend  # Trend ora crescente
+visitatori = visitatori + trend  
 
 # con np maximum qualsiasi valore al di sotto di 0 sarà 0
 visitatori = np.maximum(visitatori, 0)
+
+
+
+# Generazione di una colonna casuale per la patologia 
+patologie = ["ossa", "cuore", "testa"]
+patologia = np.random.choice(patologie, numero_giorni)
+
+# Creazione  del  DataFrame con le date come indice, il numero di visitatori e la patologia
+df = pd.DataFrame({
+    "Visitatori": visitatori.astype(int),
+    "Patologia": patologia
+}, index=date_range)
+
+print("Le prime 5 righe:")
+print(df.head())
 
